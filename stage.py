d6b113534dad44f813e3dff328de194699a57fee
Mikael Janhonen <janhon3n@gmail.com>
2017-11-06 02:12:55 +0200

    Fix errors in stage.py and move StageException to its own file

--- stage.py
+++ stage.py
@@ -1,4 +1,5 @@
 from solidObject import *
+from stageException import *
 
 class Stage:
     rowCount = 0
@@ -12,30 +13,34 @@ class Stage:
         self.state = [[None for x in range(colCount)] for y in range(rowCount)]
 
 
-    def moveObject(self, sourcePosition, destinationPosition):
-        if not issubclass(self.state[sourcePosition.row][sourcePosition.col], SolidObject):
-            raise StageException('There is no object to be moved at position ' + str(sourcePosition.row) + ',' + str(sourcePosition.col))
-        if issubclass(self.state[destinationPosition.row][destinationPosition.col], SolidObject):
-            raise StageException('There already is an object at position ' + str(destinationPosition.row) + ',' + str(destinationPosition.col))
+    def moveObject(self, object, destinationPosition):
+        sourcePosition = self.getPositionOf(object)
+        if not issubclass(type(self.state[sourcePosition['row']][sourcePosition['col']]), SolidObject):
+            raise StageException('There is no object to be moved at position ' + str(sourcePosition['row']) + ',' + str(sourcePosition['col']))
+        if issubclass(type(self.state[destinationPosition['row']][destinationPosition['col']]), SolidObject):
+            raise StageException('There already is an object at position ' + str(destinationPosition['row']) + ',' + str(destinationPosition['col']))
         
-        self.state[destinationPosition.row][destinationPosition.col] = self.state[sourcePosition.row][sourcePosition.col];
-        self.state[sourcePosition.row][sourcePosition.col] = None
+        self.state[destinationPosition['row']][destinationPosition['col']] = self.state[sourcePosition['row']][sourcePosition['col']];
+        self.state[sourcePosition['row']][sourcePosition['col']] = None
 
+    def placeObject(self, object, destinationPosition):
+        if issubclass(type(self.state[destinationPosition['row']][destinationPosition['col']]), SolidObject):
+            raise StageException('There already is an object at position ' + str(destinationPosition['row']) + ',' + str(destinationPosition['col']))
+        self.state[destinationPosition['row']][destinationPosition['col']] = object
 
     def getPositionOf(self, object):
         for i in range(0, self.rowCount):
             for j in range(0, self.colCount):
                 if self.state[i][j] is object:
-                    return GridPosition(i,j)
-        raise StageException('Given object does not exist in the stage')
+                    return { 'row':i, 'col':j }
+        return None
 
-class GridPosition:
-    row = None
-    col = None
 
-    def __init__(self, row, col):
-        self.row = row
-        self.col = col
-
-class StageException(Exception):
-    pass
+    def getObjects(self):
+        objects = []
+        for i in range(0, self.rowCount):
+            for j in range(0, self.colCount):
+                if issubclass(type(self.state[i][j]), SolidObject):
+                    dictionary = {'position': self.getPositionOf(self.state[i][j]), 'object': self.state[i][j]}
+                    objects.append(dictionary)
+        return objects
