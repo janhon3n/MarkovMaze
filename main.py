18538441fe2da50da8060667b96cbec43fdd2669
Mikael Janhonen <janhon3n@gmail.com>
2017-11-07 00:26:06 +0200

    Create Simulation class
    - Simulation class handles running a simulation on a stage
    - a GameWindow object can be added to a simulation to show its progress

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ from stage import *
 from player import *
 from bot import *
 from gameWindow import *
+from coin import *
 import time
 
 
-stage = Stage(16,22)
-player = Player(stage)
-stage.placeObject(player, {'row':1,'col':1})
+stage = Stage(8,14)
 gameWindow = GameWindow('Simulation 1', 700, 480)
+player = HumanPlayer(stage, gameWindow)
+stage.placePlayer(player, {'row':1,'col':1})
+coin = Coin(5)
+stage.placeObject(coin, {'row':3,'col':6})
+
 gameWindow.initGrid(stage)
 gameWindow.drawStage(stage)
 
-time.sleep(5)
+simulation = Simulation(stage)
+simulation.addWindow(gameWindow)
+simulation.play()
 
 #simulation = Simulation(stage, player, ai)     
---
+++ simulation.py
@@ -0,0 +1,18 @@
+from gameWindow import *
+
+class Simulation:
+    stage = None
+    gameWindow = None
+
+    def __init__(self, stage):
+        self.stage = stage
+
+    def addWindow(self, gameWindow):
+        self.gameWindow = gameWindow
+
+    def play(self):
+        while not self.stage.gameIsOver():
+            self.stage.player.move()
+
+            if self.gameWindow is not None:
+                self.gameWindow.drawStage(self.stage) 
