b8b6eeedd6982c471f9622ecb00349754b10c79b
Mikael Janhonen <janhon3n@gmail.com>
2017-11-07 00:25:08 +0200

    Create HumanPlayer class that pulls input from keyboard

--- player.py
+++ player.py
@@ -1,4 +1,23 @@
 from solidObject import *
+from msvcrt import getch
 
 class Player(SolidObject):
     points = None
+
+    def move(self):
+        return
+
+
+class HumanPlayer(Player):
+
+    gameWindow = None
+
+    def __init__(self, stage, gameWindow):
+        super().__init__(stage)
+        self.gameWindow = gameWindow
+
+    def move(self):
+        key = ""
+        while not (key == 'Up' or key == 'Left' or key == 'Down' or key == 'Right'):
+            key = self.gameWindow.checkKey()
+        self.stage.moveObjectTowardsDirection(self, key)
